3bc5f17cf35d9f471ab1c560cf4eb7d29db76d73
Mirko Siddi <mirko.siddi@gmail.com>
2020-10-25 10:00:19 +0100

    Completed

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
-'use strcict'
+"use strcict";
 
-function main () {
-  console.log('ei')
+function main() {
+  console.log("ei");
   // your code goes here.
+  let myPenguin = {
+    name: "Penguins",
+    origin: "Penguin Island",
+    author: "Anatole France",
+  };
+  console.log(`Hello, I'm a penguin and my name is ${myPenguin.name}`);
+  myPenguin.canFly = false;
 
+  myPenguin.chirp = () => {
+    console.log("CHIRP CHIRP! Is this what penguins sound like?");
+  };
+  myPenguin.sayHello = () => {
+    console.log(`Hello, I'm a penguin and my name is ${this.name}`);
+  };
+  myPenguin.sayHello();
+  myPenguin.name = "Penguin McPenguinFace";
+  myPenguin.sayHello();
+  myPenguin.fly = () => {
+    myPenguin.canFly
+      ? console.log("I can fly!")
+      : console.log("No flying for me!");
+  };
+  myPenguin.fly();
+  myPenguin.canFly = true;
+  myPenguin.fly();
+  const keysArr = Object.keys(myPenguin);
+  keysArr.forEach((elem) => console.log(elem));
+}
 
-};
-
-window.addEventListener('load', main);
+window.addEventListener("load", main);
